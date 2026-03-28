e77a36cd159393d7fc8fd4ce6443a4c814383a92
Olti100 <168911018+Olti100@users.noreply.github.com>
2026-03-28 14:32:03 +0100

    123

---
+++ module1/main.py
@@ -0,0 +1,52 @@
+print('Hello World')
+
+#string
+
+name = 'John'
+print(name)
+print(type(name))
+
+#integer
+
+age = 20
+print(age)
+print(type(age))
+
+print('My name is '+name+' and my age is'+str(age))
+
+#boolean
+
+gameState = True
+print(gameState)
+
+a = 20
+b = 3
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a ** b
+h = a%b #module
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+programmingLanguages = ['HTML', 'CSS', 'JS', 'PYTHON', 'WP']
+print(programmingLanguages)
+print(programmingLanguages[0])
+
+fruits = ['molla', 'portokalli', 'pjeshka', 'qershia']
+fruits.append('ananasi')
+
+
+print(fruits)
+
+fruits.remove('molla')
+
+print(fruits)
+
+fruits.insert(2, 'limoni')
+print(fruits)
